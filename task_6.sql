4a996e64c30a315bdad64a06578d54c1aedea811
Dennis mwangi <mwangidennis771414@gmail.com>
2024-10-20 13:55:36 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,4 +1,5 @@
 -- task_6.sql
+INSERT INTO customer", "124 Happiness  Ave.
 INSERT INTO Customers (customer_id, customer_name, email, address) VALUES
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
